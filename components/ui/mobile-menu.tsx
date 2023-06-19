39a20b65ba6b4669fd8513240cbbc9dbf7f92ddd
Cadz <cadaybelmark@gmail.com>
2023-06-19 23:31:13 +0800

    Changed the color of the mobile menu as well

--- components/ui/mobile-menu.tsx
+++ components/ui/mobile-menu.tsx
@@ -63,7 +63,7 @@ export default function MobileMenu() {
           <li>
             <Link
               href="https://docs.google.com/forms/d/e/1FAIpQLSdF6NVr8NMTyB-cZTTmdd_BCZ3wE2kBaO-IROr6RYyRQp2kIw/viewform" target="_blank"
-              className="font-medium w-full inline-flex items-center justify-center border border-transparent px-4 py-2 my-2 rounded-sm text-white bg-purple-600 hover:bg-purple-700 transition duration-150 ease-in-out" onClick={() => setMobileNavOpen(false)}
+              className="font-medium w-full inline-flex items-center justify-center border border-transparent px-4 py-2 my-2 rounded-sm text-white zerocrowdBGColor1 hover:bg-gray-900 transition duration-150 ease-in-out" onClick={() => setMobileNavOpen(false)}
             >
               Enroll now
             </Link>
